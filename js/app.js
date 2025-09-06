097222ec67ccf4ff0c82cd1acf572fad89db63a3
Anna Taulai <anna.tavlay@mail.ru>
2025-09-06 20:28:50 +0300

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// window.addEventListener('scroll', e => {
+//     document.body.style.cssText = `--scrollTop: ${this.scrollY}px`
+// })
+
+window.addEventListener('scroll', () => {
+    const scrollY = window.scrollY;
+    // Ограничиваем движение вверх, но разрешаем движение вниз
+    const limitedScroll = Math.max(0, scrollY);
+    document.body.style.setProperty('--scrollTop', `${limitedScroll}px`);
+});
