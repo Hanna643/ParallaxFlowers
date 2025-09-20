564d1f933d02a0ca6448e9550cd681a63746b161
Anna Taulai <anna.tavlay@mail.ru>
2025-09-20 19:08:03 +0300

    botim2

--- js/app.js
+++ js/app.js
@@ -8,3 +8,21 @@ window.addEventListener('scroll', () => {
     const limitedScroll = Math.max(0, scrollY);
     document.body.style.setProperty('--scrollTop', `${limitedScroll}px`);
 });
+
+
+function openBotim() {
+    // 1. Пытаемся открыть приложение напрямую
+    window.location.href = 'botim://app/chat?number=375297518854';
+    
+    // 2. Если приложения нет, через 500ms пользователь будет перенаправлен на страницу установки
+    setTimeout(function() {
+        // Проверяем, на каком устройстве находится пользователь
+        if (/iPhone|iPad|iPod/.test(navigator.userAgent)) {
+            window.location.href = 'https://apps.apple.com/app/botim/id1474264132'; // App Store
+        } else if (/Android/.test(navigator.userAgent)) {
+            window.location.href = 'https://play.google.com/store/apps/details?id=com.botim.me'; // Google Play
+        } else {
+            window.location.href = 'https://botim.me/cc/375297518854'; // Для ПК или других устройств
+        }
+    }, 500);
+}
